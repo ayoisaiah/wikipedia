5b41457228da9fd0556778f0c71372b4e7f0de17
Ayo Isaiah <sudo@ayoisaiah.com>
2017-05-08 03:23:01 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Build a  Wikipedia Viewer
+
+This is the fifth project for Free Code Camp's Front-End certificate.
+
+Completed On: 31 March 2016  
+Last Updated: 08 May 2017
+
+## Objectives
+
+Build an app that is functionally similar to this: https://codepen.io/FreeCodeCamp/full/wGqEga/.
+
+**Rule #1**: Don't look at the example project's code. Figure it out for yourself.
+
+**Rule #2**: Fulfill the below user stories. Use whichever libraries or APIs you need. Give it your own personal style.
+
+## User Stories
+
+- I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
+- I can click a button to see a random Wikipedia entry.
+
+## Hints
+
+- #1: Here's a URL you can use to get a random Wikipedia article: https://en.wikipedia.org/wiki/Special:Random.
+- #2: Here's an entry on using Wikipedia's API: https://www.mediawiki.org/wiki/API:Main_page.
+- #3: Use this link to experiment with Wikipedia's API.
+
+## My Work
+
+![Wikipedia Search Engine]()
---
+++ src/js/index.js
@@ -0,0 +1,117 @@
+"use strict";
+
+const input = document.querySelector(".searchForm-input");
+const form = document.querySelector(".searchForm");
+const loadMore = document.querySelector(".moreResults-button");
+
+const params = {
+  searchTerm: "",
+  sroffset: ""
+};
+
+function handleKeydown (event) {
+  console.log(event);
+  if (event.keyCode <= 46) return;
+  const container = document.querySelector(".container");
+  if (!container.classList.contains("top-container")) {
+    container.classList.add("top-container");
+  }
+}
+
+function handleSubmit (event) {
+
+  event.preventDefault();
+  params.searchTerm =  input.value.trim();
+  params.sroffset = "";
+
+  const resultsContainer = document.querySelector(".resultsContainer");
+  resultsContainer.innerHTML = "";
+
+  loadMore.classList.add("hidden");
+  document.querySelector(".moreResults p").classList.add("hidden")
+
+  fetchResults(params.searchTerm, params.sroffset);
+}
+
+function fetchResults (keywords, number) {
+
+  if (!keywords.length) return;
+
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${keywords}&prop=info&inprop=url&utf8=&format=json&origin=*&sroffset=${number}&srlimit=20`;
+
+  document.querySelector(".loading-indicator").classList.remove("hidden");
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+
+    document.querySelector(".loading-indicator").classList.add("hidden");
+
+    console.log(data);
+
+    if (data.query.searchinfo.totalhits) {
+      displayResults(data)
+      return;
+    }
+
+    displayErrorMessage(keywords);
+  });
+}
+
+function displayResults(data) {
+
+  const result = data.query.search;
+  params.sroffset = data.continue ? data.continue.sroffset : "";
+
+  const resultsContainer = document.querySelector(".resultsContainer");
+
+  result.forEach(result => {
+    const url = `https://en.wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`;
+
+    resultsContainer.insertAdjacentHTML("beforeend",
+      `<div class='resultItem'>
+        <h3 class='resultItem-title'>
+          <a href=${url} target='_blank'>${result.title}</a>
+        </h3>
+        <span class='resultItem-link'>${url}</span><br>
+        <span class='resultItem-snippet'>${result.snippet}</span><br>
+      </div>`
+    );
+  });
+
+  if (data.continue) {
+    loadMore.classList.remove("hidden");
+    return;
+  }
+
+  document.querySelector(".moreResults p").classList.remove("hidden")
+  loadMore.classList.add("hidden")
+
+}
+
+function displayErrorMessage (keywords) {
+
+  const resultsContainer = document.querySelector(".resultsContainer");
+  resultsContainer.innerHTML = "";
+
+  loadMore.classList.add("hidden");
+  document.querySelector(".moreResults p").classList.add("hidden")
+
+  resultsContainer.insertAdjacentHTML("beforeend",
+    `<div class='errorMessage'>
+      <p>Your search - <span class='keyword'>${keywords}</span> - did not match any documents.</p>
+        <li>Make sure that all words are spelled correctly.</li>
+        <li>Try different keywords.</li>
+        <li>Try more general keywords.</li>
+      </p>
+    </div>`
+  );
+
+}
+
+input.addEventListener("keydown", handleKeydown);
+form.addEventListener("submit", handleSubmit);
+
+loadMore.addEventListener("click", () => {
+  fetchResults(params.searchTerm, params.sroffset);
+});
